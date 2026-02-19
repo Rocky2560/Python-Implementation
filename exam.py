c28d259e8a426d6c37e43fb958b6f3cd6980a537
rocky2560 <sudipmanandhar2560@gmail.com>
2026-02-19 19:22:47 +0545

    floor, ceil, call by val or ref is called

---
+++ exam.py
@@ -0,0 +1,52 @@
+
+from math import floor, ceil
+
+a = [1,2,3]
+b= [2,3,4]
+print(a+b)
+a.extend(b)
+print(a)
+
+c =3.1
+print(floor(c))
+print(ceil(c))
+
+x=6
+y=5
+print(x/y)
+print(x//y)
+
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+def appFunc(func, a,b):
+    return func(a,b)
+print(appFunc(sub, 3,4))
+
+
+# ✅ Key Takeaways
+#
+# Numbers, strings, tuples → immutable → behave like call by value.
+#
+# Lists, dictionaries, sets → mutable → behave like call by reference.
+#
+# If you modify a mutable object inside a function, the original
+# object outside the function also changes.
+
+def call_by_val(x):
+    return x*2
+
+def call_by_ref(x):
+    x.append("D")
+    return x
+b = ["E"]
+a = 2
+
+upated_num = call_by_val(a)
+print(upated_num)
+
+updated_list = call_by_ref(b)
+print(updated_list)
